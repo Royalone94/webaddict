c4cc1d76eb2f494c2866e58fc29e43b63e5eda9f
Royalone94 <greatroyalone@outlook.com>
2022-05-09 17:22:44 -0400

    feature: implement unit test

--- movietickets/tests.py
+++ movietickets/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 # Create your tests here.
+from unittest.mock import patch
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+class MovieTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('user', 'user@example.com', 'user')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_movies(self):
+        response = self.client.get(
+            '/api/movies/',
+            format='json'
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_get_tickets(self):
+        response = self.client.get(
+            '/api/tickets/',
+            format='json'
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_get_my_tickets(self):
+        response = self.client.get(
+            '/api/tickets/get_my_tickets/',
+            format='json'
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
